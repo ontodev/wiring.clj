301ba27b8ab9f9454dca99aeda9771f0372d8d6a
chris <chris.kind.man@gmail.com>
2021-05-17 19:43:24 +0100

    Add stubs

--- src/wiring/axiomTranslation.clj
+++ src/wiring/axiomTranslation.clj
@@ -7,9 +7,9 @@
             [wiring.util :as util]
             [wiring.spec :as owlspec]))
 
+;TODO data validation
 (declare translate)
 
-;TODO
 (defn translateSubclassOf 
   "Translate a SubClassOf axiom"   
   [predicates]
@@ -17,3 +17,20 @@
         superclass (classTranslation/translate (:object predicates))]
     (util/ofsFormat "SubClassOf" subclass superclass)))
 
+(defn translateDisjointUnionOf
+  "Translate a Disjoint Union Of"
+  [predicates]
+  (let [subject (classTranslation/translate (:subject predicates))
+        arguments (classTranslation/translate (:object predicates))]
+    (util/ofsFormat "DisjointUnion" subject arguments)))
+
+(defn translate
+  "Translate predicate map to OFS."
+  [predicateMap]
+  (let [p (:predicate predicateMap)];
+    (case p
+    "rdfs:subClassOf" (translateSubclassOf predicateMap)
+    "owl:disjointUnionOf" (translateDisjointUnionOf predicateMap))))
+    ;TODO
+    ;equivalence classes (here we would need to parse multiple triples?)
+    ;disjointclasses (seems broken in gizmos)
