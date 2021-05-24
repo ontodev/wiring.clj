51f086c2d6119d6dd99513d66a7bacfc6ccc787e
chris <chris.kind.man@gmail.com>
2021-05-24 18:30:21 +0100

    Automate testing

--- src/wiring/core.clj
+++ src/wiring/core.clj
@@ -1,6 +1,7 @@
 (ns wiring.core
   (:require [clojure.repl :as repl]
             [wiring.rdf2ofsObject :as rdf2ofsObject]
+            [clojure.java.io :as io]
             [wiring.parsing :as p]
             [wiring.classTranslation :as cl]
             [wiring.axiomTranslation :as t]
@@ -11,48 +12,29 @@
   "Currently only used for manual testing."
   [& args]
 
-  ;(println (cs/generate-string {:foo "bar" :baz 5}))
-  (def testExistential "{\"rdf:type\":[{\"object\":\"owl:Restriction\"}],\"owl:onProperty\":[{\"object\":\"ex:part-of\"}],\"owl:someValuesFrom\":[{\"object\":\"ex:bar\"}]}")
+  ;(print (slurp (io/resource "tests/thickClassExpressions.txt")))
 
+  (with-open [rdr (io/reader (io/resource "tests/thickClassExpressions.txt"))]
+    (doseq [line (line-seq rdr)]
+
+      (println (str "Input: " line))
+      (println (str "Output: " (t/translate (p/parse line))))
+      (println ""))))
+
+(defn manualTesting [] 
+  ;(println (cs/generate-string {:foo "bar" :baz 5}))
+  (def testExistential "{\"rdf:type\":[{\"object\":\"owl:Restriction\"}],\"owl:onProperty\":[{\"object\":\"ex:part-of\"}],\"owl:someValuesFrom\":[{\"object\":\"ex:bar\"}]}") 
   (def testNestingIntersection  "{\"rdf:type\": [{\"object\": \"owl:Restriction\"}], \"owl:onProperty\": [{\"object\": \"ex:prop\"}], \"owl:someValuesFrom\": [{\"object\": {\"owl:intersectionOf\": [{\"object\": {\"rdf:first\": [{\"object\": \"ex:B\"}], \"rdf:rest\": [{\"object\": {\"rdf:first\": [{\"object\": \"ex:C\"}], \"rdf:rest\": [{\"object\": \"rdf:nil\"}]}}]}}], \"rdf:type\": [{\"object\": \"owl:Class\"}]}}]}'}")
   (def testNestingRestrictions "{\"rdf:type\": [{\"object\": \"owl:Restriction\"}], \"owl:onProperty\": [{\"object\": \"ex:prop\"}], \"owl:someValuesFrom\": [{\"object\": {\"rdf:type\": [{\"object\": \"owl:Restriction\"}], \"owl:onProperty\": [{\"object\": \"ex:prop\"}], \"owl:someValuesFrom\": [{\"object\": \"ex:C\"}]}}]}")
   (def testExactCardinality "{\"rdf:type\": [{\"object\": \"owl:Restriction\"}], \"owl:onProperty\": [{\"object\": \"ex:prop\"}], \"owl:cardinality\": [{\"object\": \"2^^<xsd:nonNegativeInteger\"}]}")
   (def testExactQualifiedCardinality "{\"rdf:type\": [{\"object\": \"owl:Restriction\"}], \"owl:onProperty\": [{\"object\": \"ex:prop\"}], \"owl:qualifiedCardinality\": [{\"object\": \"2^^<xmls:nonNegativeInteger\"}], \"owl:onClass\": [{\"object\": \"ex:C\"}]}")
   (def testMinCardinality "{\"rdf:type\": [{\"object\": \"owl:Restriction\"}], \"owl:onProperty\": [{\"object\": \"ex:prop\"}], \"owl:minCardinality\": [{\"object\": \"2^^<xsd:nonNegativeInteger\"}]}")
-  (def testMinQualifiedCardinality "{\"rdf:type\": [{\"object\": \"owl:Restriction\"}], \"owl:onProperty\": [{\"object\": \"ex:prop\"}], \"owl:minQualifiedCardinality\": [{\"object\": \"2^^<xmls:nonNegativeInteger\"}], \"owl:onClass\": [{\"object\": \"ex:B\"}]}")
+  (def testMinQualifiedCardinality "{\"rdf:type\": [{\"object\": \"owl:Restriction\"}], \"owl:onProperty\": [{\"object\": \"ex:prop\"}], \"owl:minQualifiedCardinality\": [{\"object\": \"2^^<xmls:nonNegativeInteger\"}], \"owl:onClass\": [{\"object\": \"ex:B\"}]}") 
+  (def testInverse "{\"rdf:type\":[{\"object\":\"owl:Restriction\"}],\"owl:onProperty\":[{\"object\":{\"owl:inverseOf\":[{\"object\":\"ex:part-of\"}]}}],\"owl:someValuesFrom\":[{\"object\":\"ex:bar\"}]}") 
+  (def subclass "{\"subject\": \"ex:A\", \"predicate\": \"rdfs:subClassOf\", \"object\": {\"rdf:type\": [{\"object\": \"owl:Restriction\"}], \"owl:onProperty\": [{\"object\": \"ex:prop\"}], \"owl:cardinality\": [{\"object\": \"2^^<xmls:nonNegativeInteger\"}]}}") 
 
-  ;;this is created MANUALLY
-  (def testInverse "{\"rdf:type\":[{\"object\":\"owl:Restriction\"}],\"owl:onProperty\":[{\"object\":{\"owl:inverseOf\":[{\"object\":\"ex:part-of\"}]}}],\"owl:someValuesFrom\":[{\"object\":\"ex:bar\"}]}")
-
-
-  (def subclass "{\"subject\": \"ex:A\", \"predicate\": \"rdfs:subClassOf\", \"object\": {\"rdf:type\": [{\"object\": \"owl:Restriction\"}], \"owl:onProperty\": [{\"object\": \"ex:prop\"}], \"owl:cardinality\": [{\"object\": \"2^^<xmls:nonNegativeInteger\"}]}}")
-
-  (def disjointclasses "{\"subject\": \"_:genid4\", \"predicate\": \"owl:AllDisjointClasses\", \"object\": {\"rdf:first\": [{\"object\": \"ex:A\"}], \"rdf:rest\": [{\"object\": {\"rdf:first\": [{\"object\": \"ex:D\"}], \"rdf:rest\": [{\"object\": {\"rdf:first\": [{\"object\": \"ex:E\"}], \"rdf:rest\": [{\"object\": {\"rdf:first\": [{\"object\": \"ex:F\"}], \"rdf:rest\": [{\"object\": \"rdf:nil\"}]}}]}}]}}]}}")
-
-
-  ;"{"rdf:type": [{"object": "owl:Restriction"}], "owl:onProperty": [{"object": "ex:prop"}], "owl:someValuesFrom": [{"object": {"rdf:type": [{"object": "owl:Restriction"}], "owl:onProperty": [{"object": "ex:prop"}], "owl:someValuesFrom": [{"object": "ex:C"}]}}]}
-
-  ;{"rdf:type":[{"object":"owl:Restriction"}],"owl:onProperty":[{"object": {"owl:inverseOf":[{"object":"ex:part-of"}]}}],"owl:someValuesFrom":[{"object":"ex:bar"}]}
-
-  (println "Test Existential: ")
-  (rdf2ofsObject/predicateMap2OFS testExistential)
-  (println "")
-
-  (println "Test Min Cardinality ")
-  (rdf2ofsObject/predicateMap2OFS testMinCardinality)
-  (println "")
-
-  (println "Test Min Qualififed Cardinality ")
-  (rdf2ofsObject/predicateMap2OFS testMinQualifiedCardinality)
-  (println "")
-
-  (println "Test Nesting ")
-  (rdf2ofsObject/predicateMap2OFS testNestingIntersection)
-  (println "")
-
-  (println "Test Nesting Restrictions")
-  (rdf2ofsObject/predicateMap2OFS testNestingRestrictions)
-  (println "")
+ (def disjointclasses "{\"subject\": \"_:genid27\", \"predicate\": \"owl:AllDisjointClasses\", \"object\": {\"owl:members\": {\"rdf:first\": [{\"object\": \"ex:disjointClass1\"}], \"rdf:rest\": [{\"object\": {\"rdf:first\": [{\"object\": \"ex:disjointClass2\"}], \"rdf:rest\": [{\"object\": {\"rdf:first\": [{\"object\": \"ex:disjointClass3\"}], \"rdf:rest\": [{\"object\": \"rdf:nil\"}]}}]}}]}}}") 
+  (def equivalentClasses "{\"subject\": \"_:genid1\", \"predicate\": \"owl:equivalentClass\", \"object\": {\"rdf:rest\": [{\"object\": {\"rdf:rest\": [{\"object\": {\"rdf:rest\": [{\"object\": \"rdf:nil\"}], \"rdf:first\": [{\"object\": \"ex:equivalent3\"}]}}], \"rdf:first\": [{\"object\": \"ex:equivalent1\"}]}}], \"rdf:first\": [{\"object\": \"ex:equivalent2\"}]}}")
 
   (println "Test spec")
   (spec/tests testExistential)
@@ -66,7 +48,18 @@
   (println (t/translate (p/parse disjointclasses)))
   (println "")
 
+  (println "Test equivalent classes ")
+  (println (t/translate (p/parse equivalentClasses)))
+  (println "")
+
+  (println "Test equivalent classes 2 ")
+  (println (t/translate (p/parse equivalentClasses2)))
+  (println "")
+
+  (println "Test equivalent classes 3 ")
+  (println (t/translate (p/parse maxCard)))
+  (println "")
+
   (println "Test Nesting ")
   (println (cl/translate (p/parse testNestingIntersection)))
-  (println "") 
-  )
+  (println ""))
