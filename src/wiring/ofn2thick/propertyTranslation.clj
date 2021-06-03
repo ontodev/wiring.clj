9913c1c6fabc2ed6e02f6c660a64a9b1cd267e02
chris <chris.kind.man@gmail.com>
2021-06-03 19:04:04 +0100

    Add translation for property expressions

---
+++ src/wiring/ofn2thick/propertyTranslation.clj
@@ -0,0 +1,25 @@
+(ns wiring.ofn2thick.propertyTranslation
+  (:require [clojure.repl :as repl]
+            [clojure.java.io :as io]
+            [wiring.thick2ofn.spec :as spec])
+  (:gen-class))
+
+;TODO data validation
+(declare translate) 
+
+(defn translateInverseOf 
+  "Translate ObjectInverseOf expression"
+  [ofn]
+  (let [[op arg] ofn
+        inverse "{\"owl:inverseOf\": "
+        operand (str inverse "[{\"object\": " (translate arg) "}]")
+        closing (str operand "}")]
+    closing))
+
+(defn translate 
+  "Translate OFN-S property expression into predicate map"
+  [ofn]
+  (println ofn)
+  (if (= "ObjectInverseOf" (first ofn))
+    (translateInverseOf ofn)
+    (str "\"" ofn "\"")))
