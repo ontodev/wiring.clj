afbb67dfbb1116522c580866a89c3347facc6885
chris <chris.kind.man@gmail.com>
2021-06-05 19:47:00 +0100

    Add (prototype) translation for class expressions

---
+++ src/wiring/ofn2man/classTranslation.clj
@@ -0,0 +1,166 @@
+(ns wiring.ofn2man.classTranslation
+  (:require [clojure.repl :as repl]
+            [clojure.java.io :as io])
+            ;[wiring.ofn2thick.propertyTranslation :as property]
+            ;[wiring.thick2ofn.spec :as spec])
+  (:gen-class))
+
+;TODO pretty printing with line breaks and indentations
+
+;TODO data validation
+(declare translate) 
+
+;TODO nested property expressions
+(defn namedClass?
+  "Checks whether an expression is a named class."
+  [ofn]
+  (let [operator (first ofn)];
+    (case operator
+      "ObjectSomeValuesFrom" false
+      "ObjectAllValuesFrom" false
+      "ObjectHasValue"  false
+      "ObjectMinCardinality" false
+      "ObjectMaxCardinality" false
+      "ObjectExactCardinality" false
+      "ObjectIntersectionOf" false
+      "ObjectUnionOf" false
+      "ObjectOneOf" false
+      "ObjectComplementOf" false
+      true)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                      Restrictions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn translateObjectSomeValuesFrom
+  "Translate a ObjectSomeValuesFrom expression"
+  [ofn]
+  (let [[op property filler] ofn]
+    (if (namedClass? filler)
+        (str property " some " filler)
+        (str property " some (" (translate filler) ")")))) 
+
+(defn translateObjectAllValuesFrom
+  "Translate a ObjectAllValuesFrom expression"
+  [ofn]
+  (let [[op property filler] ofn]
+    (if (namedClass? filler)
+        (str property " only " filler)
+        (str property " only (" (translate filler) ")")))) 
+
+(defn translateObjectHasValue
+  "Translate a ObjectHasValue expression"
+  [ofn]
+  (let [[op property filler] ofn]
+        (str property " value " filler)));no nesting for filler because it's an individual
+
+(defn translateObjectMinUnqualifiedCardinality
+  "Translate a ObjectMinCardinality expression"
+  [ofn]
+  (let [[op cardinality property] ofn] 
+        (str property " min " cardinality " owl:Thing")))
+
+(defn translateObjectMinQualifiedCardinality
+  "Translate a ObjectMinQualifiedCardinality expression"
+  [ofn]
+  (let [[op cardinality property filler] ofn]
+    (if (namedClass? filler)
+        (str property " min " cardinality " " filler)
+        (str property " min " cardinality " (" (translate filler) ")")))) 
+
+(defn translateObjectMinCardinality
+  "Translate a ObjectMinCardinality expression"
+  [ofn]
+  (if (= 3 (count ofn))
+    (translateObjectMinUnqualifiedCardinality ofn)
+    (translateObjectMinQualifiedCardinality ofn)))
+
+(defn translateObjectMaxUnqualifiedCardinality
+  "Translate a ObjectMaxCardinality expression"
+  [ofn]
+  (let [[op cardinality property] ofn] 
+        (str property " max " cardinality " owl:Thing")))
+
+(defn translateObjectMaxQualifiedCardinality
+  "Translate a ObjectMaxQualifiedCardinality expression"
+  [ofn]
+  (let [[op cardinality property filler] ofn]
+    (if (namedClass? filler)
+        (str property " max " cardinality " " filler)
+        (str property " max " cardinality " (" (translate filler) ")")))) 
+
+(defn translateObjectMaxCardinality
+  "Translate a ObjectMaxCardinality expression"
+  [ofn]
+  (if (= 3 (count ofn))
+    (translateObjectMaxUnqualifiedCardinality ofn)
+    (translateObjectMaxQualifiedCardinality ofn)))
+
+(defn translateObjectExactUnqualifiedCardinality
+  "Translate a ObjectExactCardinality expression"
+  [ofn]
+  (let [[op cardinality property] ofn] 
+        (str property " exactly " cardinality " owl:Thing")))
+
+(defn translateObjectExactQualifiedCardinality
+  "Translate a ObjectExactQualifiedCardinality expression"
+  [ofn]
+  (let [[op cardinality property filler] ofn]
+    (if (namedClass? filler)
+        (str property " exactly " cardinality " " filler)
+        (str property " exactly " cardinality " (" (translate filler) ")")))) 
+
+(defn translateObjectExactCardinality
+  "Translate a ObjectExactCardinality expression"
+  [ofn]
+  (if (= 3 (count ofn))
+    (translateObjectExactUnqualifiedCardinality ofn)
+    (translateObjectExactQualifiedCardinality ofn)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;             Propositional Connectives
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn translateObjectIntersection
+  "Translate an ObjectIntersectionOf expression"
+  [ofn]
+  (let [[operator & arguments] ofn
+        args (map #(if (namedClass? %) % (str "(" (translate %) ")")) arguments)]
+    (apply str (interpose " and " args))))
+
+(defn translateObjectUnion
+  "Translate an ObjectUnionOf expression"
+  [ofn]
+  (let [[operator & arguments] ofn
+        args (map #(if (namedClass? %) % (str "(" (translate %) ")")) arguments)]
+    (apply str (interpose " or " args))))
+
+(defn translateObjectOneOf
+  "Translate an ObjectOneOf expression"
+  [ofn]
+  (let [[operator & arguments] ofn]
+   (str "{" (apply str (interpose " , " arguments)) "}")))
+
+(defn translateObjectComplement
+  "Translate an ObjectComplementOf expression"
+  [ofn]
+  (let [[operator argument] ofn]
+    (str "not (" (translate argument) ")")));NB: there are parenthesis for named classes too
+
+(defn translate
+  "Translate OFN-S expression to manchester synax"
+  [ofn]
+  (let [operator (first ofn)];
+    (case operator
+      "ObjectSomeValuesFrom"  (translateObjectSomeValuesFrom ofn)
+      "ObjectAllValuesFrom"  (translateObjectAllValuesFrom ofn)
+      "ObjectHasValue"  (translateObjectHasValue ofn)
+      "ObjectMinCardinality"  (translateObjectMinCardinality ofn)
+      "ObjectMaxCardinality"  (translateObjectMaxCardinality ofn)
+      "ObjectExactCardinality"  (translateObjectMaxCardinality ofn)
+      "ObjectIntersectionOf"  (translateObjectIntersection ofn)
+      "ObjectUnionOf"  (translateObjectUnion ofn)
+      "ObjectOneOf"  (translateObjectOneOf ofn)
+      "ObjectComplementOf"  (translateObjectComplement ofn)
+      ofn)))
+
