d3898b6caac4e3c33398bced6485529f87c00191
chris <chris.kind.man@gmail.com>
2021-06-08 11:29:17 +0100

    Add handling of (standard) namespaces

--- src/wiring/thick2ofn/parsing.clj
+++ src/wiring/thick2ofn/parsing.clj
@@ -5,8 +5,14 @@
             [clojure.spec.alpha :as spec]
             [wiring.thick2ofn.spec :as owlspec]))
 
-;TODO handle single quotation marks {'subject': 'ex:A', 'predicate': 'rdf:type', 'object': 'owl:Class'} 
-;TODO: handle quotations around thick map after 'object' 
+(defn handleNamespaces
+  "Replace standard namespace IRI's with their name prefixes"
+  [input]
+  (let [owl (s/replace input #"http://www.w3.org/2002/07/owl#" "owl:")
+        rdf (s/replace owl #"http://www.w3.org/1999/02/22-rdf-syntax-ns#" "rdf:")
+        rdfs (s/replace rdf #"http://www.w3.org/2000/01/rdf-schema#" "rdfs:")
+        xsd (s/replace rdfs #"http://www.w3.org/2001/XMLSchema#" "xsd:")]
+    xsd))
 
 (defn parse
   "Parse JSON predicate map."
@@ -15,5 +21,6 @@
    :post [(spec/valid? ::owlspec/map %)]}
   (let [removedObject (s/replace predicateMap #"\[\{\"object\":" "")
         removedClosingBracket (s/replace removedObject  #"\}\]" "")
-        predicates (cs/parse-string removedClosingBracket true)]
+        namespacePrefixes (handleNamespaces removedClosingBracket) 
+        predicates (cs/parse-string namespacePrefixes  true)]
     predicates))
