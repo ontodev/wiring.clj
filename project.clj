f2b4d51ccc7bba44c93a3a69bcdfd472eaf7121a
chris <chris.kind.man@gmail.com>
2021-07-12 12:43:38 +0100

    Add hiccup dependency

--- project.clj
+++ project.clj
@@ -5,13 +5,16 @@
             :url "https://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [cheshire "5.10.0"]
+                 [hiccup "1.0.5"]
                  [instaparse "1.4.10"]]
   :plugins [[lein-cljfmt "0.7.0"]] 
-  :main ^:skip-aot wiring.thick2ofn.core
+  :main ^:skip-aot wiring.ofn2hiccup.core
+  ;:main ^:skip-aot wiring.thick2ofn.core
   ;:main ^:skip-aot wiring.ofn2thick.core
   ;:main ^:skip-aot wiring.ofn2man.core
   ;:main ^:skip-aot wiring.cli.core
   ;:main ^:skip-aot wiring.thick2owl.core
+  ;:main ^:skip-aot wiring.ofn2rdfa.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
