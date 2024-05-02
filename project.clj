eed1945df2f7ea3a5e06a3ef2080a77b5915ae01
ckindermann <chris.kind.man@gmail.com>
2024-05-01 21:24:01 -0700

    Add dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wiring "0.1.0-SNAPSHOT"
+(defproject wiring "0.1.0-SNAPSHOT" 
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "BSD 3-Clause License"
@@ -6,14 +6,12 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [cheshire "5.10.0"]
                  [hiccup "1.0.5"]
-                 [instaparse "1.4.10"]]
+                 [instaparse "1.4.10"]
+                 [org.clojure/java.jdbc "0.7.12"]
+                 [org.xerial/sqlite-jdbc "3.36.0"]
+                 [org.clojure/data.csv "1.0.0"]]
   :plugins [[lein-cljfmt "0.7.0"]] 
-  ;:main ^:skip-aot wiring.thick2ofn.core
-  ;:main ^:skip-aot wiring.ofn2thick.core
-  ;:main ^:skip-aot wiring.ofn2man.core
-  ;:main ^:skip-aot wiring.cli.core
-  ;:main ^:skip-aot wiring.thick2owl.core
-  :main ^:skip-aot wiring.ofn2rdfa.core
+  :main ^:skip-aot wiring.ldtab2ofn.axiomTranslation.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
