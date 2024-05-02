a5f705966f78d5bbc9ee8519ee3b0889c49b6837
ckindermann <chris.kind.man@gmail.com>
2024-05-01 21:40:02 -0700

    Format

--- src/wiring/typing/core.clj
+++ src/wiring/typing/core.clj
@@ -10,20 +10,18 @@
 
 (defn -main
   "Currently only used for manual testing."
-  [& args] 
+  [& args]
 
   ;translate ontology given via thick triples into OFN-S expressions
   (def ofnExpressions (->> "tests/thickOBO.txt"
-       io/resource
-       io/reader 
-       line-seq
-       (map p/parse)
-       (map thick2ofn/translate)))
+                           io/resource
+                           io/reader
+                           line-seq
+                           (map p/parse)
+                           (map thick2ofn/translate)))
 
   (println ofnExpressions)
   (println "")
 
   (def typing (typeHandling/typingUpdate ofnExpressions))
-  (println typing)
-
-  )
+  (println typing))
